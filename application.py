4579d7b94acdbf5b077300017e2b4c3a81db903d
MatteoLevy <levymatteo@gmail.com>
2024-04-03 00:38:34 +0200

    Added fallbacks different cases with while loop

--- application.py
+++ application.py
@@ -1,9 +1,16 @@
-start_workout = input('Start workout (Y/N)? ').upper()
-print(start_workout)
+# Verify that the input is among the valid options, throws error otherwise and asks for input again.
+start_workout_choices = ["Y", "N"]
+while True:
+    start_workout = input('Start workout (Y/N)? ').upper()
+    if start_workout in start_workout_choices:
+        break
+    else:
+        print('Error! Value not allowed! It must be "Y" or "N"')
 
 if start_workout == 'Y':
     exercise = input('Exercise? ')
     while True:
+        # Verify that the reps input is an integer, throws error otherwise and asks for input again.
         while True:
             try: 
                 reps = int(input('Reps? '))
@@ -11,7 +18,16 @@ if start_workout == 'Y':
             except ValueError: 
                 print('Error! Insert an integer number (e.g. \'8\' or \'12\')')
                 continue
-        choice = input('More reps (R), new exercise (N) or end workout (Q)? ').upper()
+        
+        # Verify that the input is among the valid options, throws error otherwise and asks for input again.
+        valid_choices = ["R", "N", "Q"]
+        while True:
+            choice = input('More reps (R), new exercise (N), or end workout (Q)? ').upper()
+            if choice in valid_choices:
+                break
+            else:
+                print('Error! Value not allowed! It must be "R", "N", or "Q"')
+
         if choice == 'R':
             continue
         elif choice == 'N': 
@@ -19,7 +35,7 @@ if start_workout == 'Y':
         elif choice == 'Q': 
             break
         else: 
-            print('Error! Value not allowed! It must be "R", "N" or "Q"')
+            print('Error!')
     print('Well done!')
 else: 
     print('Alright, see you next time!')
