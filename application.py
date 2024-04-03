9711ffa8e0c5438a613c10a782fdbc5d770e97a8
MatteoLevy <levymatteo@gmail.com>
2024-04-03 23:39:14 +0200

    Added logic to create tables in SQLite DB and populate Workout table once session is started

--- application.py
+++ application.py
@@ -1,3 +1,30 @@
+import sqlite3
+from datetime import datetime
+
+conn = sqlite3.connect('workouts.db')
+cur = conn.cursor()
+current_time = datetime.now()
+
+# Create Workouts, Sets and Exercises tables, if they do not alrady exist
+cur.executescript('''
+CREATE TABLE IF NOT EXISTS Workouts(
+                  id INTEGER PRIMARY KEY,
+                  date TEXT
+);
+
+CREATE TABLE IF NOT EXISTS Sets(
+                  id INTEGER PRIMARY KEY,
+                  workout_id INTEGER,
+                  count INTEGER
+);                
+
+CREATE TABLE IF NOT EXISTS Exercises(
+                  id INTEGER PRIMARY KEY,
+                  name TEXT,
+                  set_id INTEGER
+);                         
+''')
+
 # Verify that the input is among the valid options, throws error otherwise and asks for input again.
 start_workout_choices = ["Y", "N"]
 while True:
@@ -8,6 +35,20 @@ while True:
         print('Error! Value not allowed! It must be "Y" or "N"')
 
 if start_workout == 'Y':
+    
+    # Insert a new row in the Workouts table
+    cur.execute('''INSERT OR IGNORE INTO Workouts (date)
+                VALUES ( ? )''', (current_time, ))
+    
+    conn.commit()
+
+    # Provides the ID of the Workout created in the Workputs table
+    cur.execute('SELECT id FROM Workouts WHERE date = ? ', (current_time, ))
+    workout_id = cur.fetchone()[0]
+    print("Workout ID: ", workout_id)
+
+    conn.close()
+
     exercise = input('Exercise? ')
     while True:
         # Verify that the reps input is an integer, throws error otherwise and asks for input again.
