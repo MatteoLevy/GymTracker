0a782dfacf6a0bcb34ab0076a525c4360c72495d
MatteoLevy <levymatteo@gmail.com>
2024-04-06 00:46:44 +0200

    Fixed small bug with wrong variable name for reps causing issues with db insert. Added validation reps as int in sub-iteration case (I only added to the main iteration with last commit). Also moved close  db connection to end of the script.

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 conn = sqlite3.connect('workouts.db')
 cur = conn.cursor()
-current_time = datetime.now()
+current_time = datetime.now().isoformat()
 
 # Create Workouts, Sets and Exercises tables, if they do not alrady exist
 cur.executescript('''
@@ -47,7 +47,15 @@ if start_workout == 'Y':
     while True:
         sets_counter += 1
         exercise = input('Exercise? ')
-        reps_counter = int(input('Reps? '))
+
+        # Verify that the reps input is an integer, throws error otherwise and asks for input again.
+        while True:
+            try: 
+                reps_counter = int(input('Reps? '))
+                break
+            except ValueError: 
+                print('Error! Insert an integer number (e.g. \'8\' or \'12\')')
+                continue
 
         # Insert a new row in the Exercises table
         cur.execute('''INSERT OR IGNORE INTO Exercises (name, workout_id, sets_counter, reps_counter)
@@ -60,11 +68,25 @@ if start_workout == 'Y':
         exercise_id = cur.fetchone()[0]
         print("Exercise ID: ", exercise_id)
 
-        choice = input('More reps (R), new exercise (N), or end workout (Q)? ').upper()
+        # Verify that the input is among the valid options, throws error otherwise and asks for input again.
+        valid_choices = ["R", "N", "Q"]
+        while True:
+            choice = input('More reps (R), new exercise (N), or end workout (Q)? ').upper()
+            if choice in valid_choices:
+                break
+            else:
+                print('Error! Value not allowed! It must be "R", "N", or "Q"')
 
         while choice == "R":
             sets_counter += 1
-            reps_counter = int(input('Reps? '))
+            # Verify that the reps input is an integer, throws error otherwise and asks for input again.
+            while True:
+                try: 
+                    reps_counter = int(input('Reps? '))
+                    break
+                except ValueError: 
+                    print('Error! Insert an integer number (e.g. \'8\' or \'12\')')
+                    continue
 
             # Insert a new row in the Exercises table
             cur.execute('''INSERT OR IGNORE INTO Exercises (name, workout_id, sets_counter, reps_counter)
@@ -77,7 +99,12 @@ if start_workout == 'Y':
             exercise_id = cur.fetchone()[0]
             print("Exercise ID: ", exercise_id)
 
-            choice = input('More reps (R), new exercise (N), or end workout (Q)? ').upper()
+            while True:
+                choice = input('More reps (R), new exercise (N), or end workout (Q)? ').upper()
+                if choice in valid_choices:
+                    break
+                else:
+                    print('Error! Value not allowed! It must be "R", "N", or "Q"')
 
             if choice != 'R':
                 sets_counter = 0
@@ -90,8 +117,8 @@ if start_workout == 'Y':
     
     print('Well done!')
 
-    conn.close()
-
 else: 
     print('Alright, see you next time!')
     quit()
+
+conn.close()
