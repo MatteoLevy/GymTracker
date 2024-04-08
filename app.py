da2e068f5958cb645a0ba0e168ad7447f1fb218a
MatteoLevy <levymatteo@gmail.com>
2024-04-08 22:46:29 +0200

    Created 'DatabaseHandler' class, replacing functions from previous 'dataHandler' file containing only functions. Updated 'app.py' with logic from the new class

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 import sqlite3
 from datetime import datetime
-from utils.dataHandler import create_tables, insert_workout
+from utils.DataHandler import DataHandler
 from utils.inputValidator import get_string_input, get_integer_input
-
 
 conn = sqlite3.connect("workouts.db")
 cur = conn.cursor()
 current_time = datetime.now().isoformat()
 
 
-create_tables()
-
-
 def main():
-    create_tables()
+
+    # Initialize database and database commands
+    data_handler = DataHandler("workouts.db")
+
+    # Create tables if not already existing
+    data_handler.create_table("Workouts", id="INTEGER PRIMARY KEY", date="TEXT")
+    data_handler.create_table(
+        "Exercises", id="INTEGER PRIMARY KEY", workout_id="INTEGER", name="TEXT"
+    )
+    data_handler.create_table(
+        "Sets",
+        id="INTEGER PRIMARY KEY",
+        exercise_id="INTEGER",
+        sets_counter="INTEGER",
+        reps_counter="INTEGER",
+    )
+
     start_workout = get_string_input("Start workout (Y/N)? ", ["Y", "N"])
     if start_workout == "Y":
-        insert_workout()
+        # Insert a new workout in the Workouts table
+        data_handler.insert_into_table("Workouts", date=current_time)
+
         cur.execute("SELECT id FROM Workouts WHERE date = ? ", (current_time,))
         workout_id = cur.fetchone()[0]
         sets_counter = 0
@@ -27,13 +41,9 @@ def main():
             reps_counter = get_integer_input("Reps? ")
 
             # Insert a new row in the Exercises table
-            cur.execute(
-                """INSERT OR IGNORE INTO Exercises (workout_id, name)
-                        VALUES ( ? , ? )""",
-                (workout_id, exercise),
+            data_handler.insert_into_table(
+                "Exercises", workout_id=workout_id, name=exercise
             )
-
-            conn.commit()
 
             # Provides the ID of the Exercise created in the Exercises table
             cur.execute(
@@ -43,13 +53,12 @@ def main():
             exercise_id = cur.fetchone()[0]
 
             # Insert a new row in the Sets table
-            cur.execute(
-                """INSERT OR IGNORE INTO Sets (exercise_id, sets_counter, reps_counter)
-                        VALUES ( ? , ? , ? )""",
-                (exercise_id, sets_counter, reps_counter),
+            data_handler.insert_into_table(
+                "Sets",
+                exercise_id=exercise_id,
+                sets_counter=sets_counter,
+                reps_counter=reps_counter,
             )
-
-            conn.commit()
 
             choice = get_string_input(
                 "More reps (R), new exercise (N), or end workout (Q)? ", ["R", "N", "Q"]
@@ -59,13 +68,9 @@ def main():
                 reps_counter = get_integer_input("Reps? ")
 
                 # Insert a new row in the Exercises table
-                cur.execute(
-                    """INSERT OR IGNORE INTO Exercises (workout_id, name)
-                            VALUES ( ? , ? )""",
-                    (workout_id, exercise),
+                data_handler.insert_into_table(
+                    "Exercises", workout_id=workout_id, name=exercise
                 )
-
-                conn.commit()
 
                 # Provides the ID of the Exercise created in the Exercises table
                 cur.execute(
@@ -75,13 +80,12 @@ def main():
                 exercise_id = cur.fetchone()[0]
 
                 # Insert a new row in the Sets table
-                cur.execute(
-                    """INSERT OR IGNORE INTO Sets (exercise_id, sets_counter, reps_counter)
-                            VALUES ( ? , ? , ? )""",
-                    (exercise_id, sets_counter, reps_counter),
+                data_handler.insert_into_table(
+                    "Sets",
+                    exercise_id=exercise_id,
+                    sets_counter=sets_counter,
+                    reps_counter=reps_counter,
                 )
-
-                conn.commit()
 
                 choice = get_string_input(
                     "More reps (R), new exercise (N), or end workout (Q)? ",
---
+++ utils/DatabaseHandler.py
@@ -0,0 +1,43 @@
+import sqlite3
+
+
+class DataHandler:
+    def __init__(self, db_name):
+        self.db_name = db_name
+        self.conn = sqlite3.connect(db_name)
+        self.cur = self.conn.cursor()
+
+    # Method to create a table
+    def create_table(self, table_name, **columns):
+        try:
+            # Columns is a dictionary containing column names as keys and data types as values
+            column_definitions = ", ".join(
+                [f"{column} {data_type}" for column, data_type in columns.items()]
+            )
+            create_table_query = (
+                f"CREATE TABLE IF NOT EXISTS {table_name} ({column_definitions})"
+            )
+            self.cur.execute(create_table_query)
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print(f"Error creating table {table_name}: {error}")
+
+    # Example create_table
+    # data_handler = DataHandler("workouts.db")
+    # data_handler.create_table("Workouts", id="INTEGER PRIMARY KEY", date="TEXT")
+
+    # Insert new row(s) in a table
+    def insert_into_table(self, table_name, **data):
+        try:
+            # Columns is a dictionary containing column names as keys and data types as values
+            columns = ", ".join(data.keys())
+            question_marks = ", ".join("?" * len(data))
+            insert_query = f"INSERT OR IGNORE INTO {table_name} ({columns}) VALUES ({question_marks})"
+            self.cur.execute(insert_query, tuple(data.values()))
+            self.conn.commit()
+        except sqlite3.Error as error:
+            print(f"Error inserting row into table {table_name}: {error}")
+
+    # Example insert_into_table
+    # data_handler = DataHandler("workouts.db")
+    # data_handler.insert_into_table("Workouts", date=current_time)
